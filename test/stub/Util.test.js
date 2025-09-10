814c1574f6064d7b05110ec6537a242419e01c4c
jim <waczzrr@163.com>
2025-09-11 02:47:55 +0800

    1

--- test/stub/Util.test.js
+++ test/stub/Util.test.js
@@ -11,12 +11,19 @@ describe('test Util', () => {
         util = null
     })
     it("test add without step", function () {
+        let add = sinon.stub(util, 'add')
+        add();
+        sinon.assert.callCount(add, 1);
+        sinon.assert.calledOnce(add);
+        // sinon.assert.calledWith(add, 1);
+        sinon.assert.returns(add, 10);
+        add.restore();
+    })
+    it("test fn1", function () {
         const UtilStub = sinon.createStubInstance(Util);
-        let log = sinon.spy(console, 'log');
         UtilStub.fn1();
         UtilStub.fn2();
         sinon.assert.calledOnce(UtilStub.fn1);
         sinon.assert.calledOnce(UtilStub.fn2);
-        sinon.assert.callCount(log, 2);
     })
 });
